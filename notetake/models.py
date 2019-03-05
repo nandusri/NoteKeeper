70bc8b9ec7d1a4a1b6ca09e2857b735881db351a
nandusri <kasu.nandu@gmail.com>
2019-03-05 16:49:23 +0530

    first commit

---
+++ notetake/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Note(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_on = models.DateTimeField(auto_now=True)
+	updated_on = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+	def __str__(self):
+		return self.title
