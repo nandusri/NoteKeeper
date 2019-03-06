f52c60b340e225f02293633a61f8bf12aff01273
nandusri <kasu.nandu@gmail.com>
2019-03-06 17:19:06 +0530

    some changes are made here

--- notetake/urls.py
+++ notetake/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('dashboard/',views.note_list, name="note_list"),
-	
+	path('note/<int:pk>/', views.note_detail, name='note_detail'),
 ]
--- notetake/views.py
+++ notetake/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Note
 
 def note_list(request):
 	notes = Note.objects.all().order_by('-created_on')
 	return render(request, 'notetake/note_list.html',{'notes':notes})
-	
+def note_detail(request,pk):
+	note = get_object_or_404(Note, pk=pk)
+	return render(request, 'notetake/note_detail.html', {'note': note})
+		
