395c34c4e68fe2ea46b18bba5b17867d265cdbe7
nandusri <kasu.nandu@gmail.com>
2019-03-06 12:37:29 +0530

    some changes are made here

--- notetake/urls.py
+++ notetake/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('dashboard/',views.note_list, name="note_list"),
+	
 ]
--- notetake/views.py
+++ notetake/views.py
@@ -4,5 +4,4 @@ from .models import Note
 def note_list(request):
 	notes = Note.objects.all().order_by('-created_on')
 	return render(request, 'notetake/note_list.html',{'notes':notes})
-
 	
