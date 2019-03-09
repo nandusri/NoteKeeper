086936d6297269f751d9ed9683867122b48cba28
nandusri <kasu.nandu@gmail.com>
2019-03-09 17:37:20 +0530

    new note added here

--- notetake/urls.py
+++ notetake/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	path('dashboard/',views.note_list, name="note_list"),
 	path('note/<int:pk>/', views.note_detail, name='note_detail'),
+	path('note/new/', views.note_new, name='note_new'),
 ]
--- notetake/views.py
+++ notetake/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Note
+from .forms import NoteForm
 
 def note_list(request):
 	notes = Note.objects.all().order_by('-created_on')
@@ -7,4 +8,7 @@ def note_list(request):
 def note_detail(request,pk):
 	note = get_object_or_404(Note, pk=pk)
 	return render(request, 'notetake/note_detail.html', {'note': note})
+def note_new(request):
+	form = NoteForm()
+	return render(request, 'notetake/note_edit.html', {'form': form})
 		
