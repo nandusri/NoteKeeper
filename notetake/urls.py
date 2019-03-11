73dfcf7db6a6785da712d889758971e5eabae1c7
nandusri <kasu.nandu@gmail.com>
2019-03-11 23:16:35 +0530

    new feature add

--- notetake/urls.py
+++ notetake/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path('dashboard/',views.note_list, name="note_list"),
 	path('note/<int:pk>/', views.note_detail, name='note_detail'),
 	path('note/new/', views.note_new, name='note_new'),
+	path('note/<int:pk>/edit', views.note_edit, name='note_edit'),
 ]
--- notetake/views.py
+++ notetake/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Note
 from .forms import NoteForm
+
 
 def note_list(request):
 	notes = Note.objects.all().order_by('-created_on')
@@ -9,6 +10,26 @@ def note_detail(request,pk):
 	note = get_object_or_404(Note, pk=pk)
 	return render(request, 'notetake/note_detail.html', {'note': note})
 def note_new(request):
-	form = NoteForm()
+	if request.method == "POST":
+		form = NoteForm(request.POST)
+		if form.is_valid():
+			note = form.save(commit=False)
+			note.user = request.user
+			note.save()
+			return redirect('note_detail', pk=note.pk)
+	else:
+		form = NoteForm()
 	return render(request, 'notetake/note_edit.html', {'form': form})
+def note_edit(request, pk):
+	note = get_object_or_404(Note, pk=pk)
+	if request.method == "POST":
+		form = NoteForm(request.POST, instance=post)
+		if form.is_valid():
+			note = form.save(commit=False)
+			note.user = request.user
+			note.save()
+			return redirect('note_detail', pk=note.pk)
+	else:
+		form = NoteForm(instance=post)
+	return render(request, 'notetake/note_edit.html', {'form': form})	
 		
