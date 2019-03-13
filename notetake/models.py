24e81f75b8821cb3eae0b1624f158edecca4f5d4
nandusri <kasu.nandu@gmail.com>
2019-03-13 23:27:22 +0530

    markdown added

--- notetake/models.py
+++ notetake/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.utils.html import mark_safe
+from markdown import markdown
 
 class Note(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -11,3 +13,7 @@ class Note(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_content_as_markdown(self):
+  		return mark_safe(markdown(self.text, safe_mode='escape'))
+        
