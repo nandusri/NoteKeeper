0311c4a7891d2de0771ce8e48223b05a32ec4da9
nandusri <kasu.nandu@gmail.com>
2019-03-13 19:09:50 +0530

    small change.

--- notetake/views.py
+++ notetake/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login
 
 @login_required
 def note_list(request):
-	notes = Note.objects.all().order_by('-created_on')
+	notes = Note.objects.filter(user=request.user).order_by('-created_on')
 	return render(request, 'notetake/note_list.html',{'notes':notes})
 @login_required
 def note_detail(request,pk):
