27c3723e9a07c632d154338bbb69b990faf72b84
Nandini Kasu <nandini.kasu@vtfintech.com>
2019-10-24 08:13:06 +0530

    small changes are added

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('', include('notetake.urls'))
 ]
